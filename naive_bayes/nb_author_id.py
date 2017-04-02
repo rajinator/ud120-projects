0492c9aeaa4174ea4e5b494be039f197f95a7219
rajinator <rajinator@users.noreply.github.com>
2017-04-02 13:01:21 -0400

    Completed Naive Bayes miniproject - used GaussianNB() fit and predict methods, timed training vs prediction

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -11,7 +11,8 @@
 """
     
 import sys
-from time import time
+import time
+
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
@@ -22,10 +23,25 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
-
 #########################################################
 ### your code goes here ###
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
+t0 = time.time()
+clf.fit(features_train, labels_train)
+print "training time for clf fit:", round(time.time()-t0, 3), "s"
+
+t0 = time.time()
+pred = clf.predict(features_test)
+print "training time for clf prediction:", round(time.time()-t0, 3), "s"
+
+from sklearn.metrics.scorer import accuracy_score
+
+accuracy = accuracy_score(pred,labels_test)
+
+print(accuracy)
+
+
 
 
 #########################################################
